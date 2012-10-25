26200f26b26c641fec2a3837b9725dbb1ce911d6
Theodor Müller <theodor.mueller@kuurt.de>
2012-10-25 16:46:20 +0200

    Added Target for iOS Library

---
+++ YAML-iOS/YAML_iOS.h
@@ -0,0 +1,13 @@
+//
+//  YAML_iOS.h
+//  YAML-iOS
+//
+//  Created by Theodor Müller on 25.10.12.
+//
+//
+
+#import <Foundation/Foundation.h>
+
+@interface YAML_iOS : NSObject
+
+@end
---
+++ YAML-iOS/YAML_iOS.m
@@ -0,0 +1,13 @@
+//
+//  YAML_iOS.m
+//  YAML-iOS
+//
+//  Created by Theodor Müller on 25.10.12.
+//
+//
+
+#import "YAML_iOS.h"
+
+@implementation YAML_iOS
+
+@end
